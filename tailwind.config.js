a6caa9437bda1b3d1d979f3507a92ec9571d738f
Ashley Connor <me@ashleyconnor.co.uk>
2021-06-01 12:53:49 -0400

    Add box shadow border to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      "boxShadow": {
+        "border": "inset 0 -1px 0 0 rgba(101,109,119,.16)"
+      }
+    },
   },
   variants: {
     extend: {},
